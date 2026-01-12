79276cf1a31b56d810af313230c946f2265e6e93
sofiane <sofianlkhassibi10@gmail.com>
2026-01-12 19:00:06 +0100

    s

---
+++ view/logingsystem/regester.php
@@ -0,0 +1,29 @@
+<php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+<div class="main">
+    <h1>create ur admine account</h1>
+    <form action="">
+        
+
+
+
+    
+    </form>
+</div>
+    
+</body>
+</html>
